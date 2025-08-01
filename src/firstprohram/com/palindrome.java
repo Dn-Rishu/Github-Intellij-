32f03ce429df69f8dbe7c9d6de4c307e5ddb8b81
palindrome <dnrishu3092@gmail.com>
2025-08-01 13:40:16 +0530

    init commit

--- src/firstprohram/com/palindrome.java
+++ src/firstprohram/com/palindrome.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class palindrome {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter the ori")
         int num=sc.nextInt();
         int ori=num;
         int rev=0;
